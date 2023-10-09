36924828ca02eb03852bbaea461d234e8145e176
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-09 15:04:14 +0600

    update navbar section

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
     return (
-        <div>
-            <h3>This is navbar section</h3>
+        <div className="flex justify-around p-7">
+            <Link to="/"><h3 className="text-3xl font-bold ">CareerHub</h3></Link>
+            <div className="grid grid-cols-3 gap-10">
+                <Link to="/statistics"><h3 className="text-lg text-blue-600">Statistics</h3></Link>
+                <Link to="/appliedJobs"><h3 className="text-lg">Applied Jobs</h3></Link>
+                <Link to="/blog"><h3 className="text-lg">Blog</h3></Link>
+            </div>
+            <button className="px-6 py-2 rounded-lg text-white text-center bg-indigo-600">Star Applying</button>
         </div>
     );
 };
