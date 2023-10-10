5f60a65e9fb77c40c3de2fef548080fbf75eb734
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-10 17:25:50 +0600

    update blog section

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,17 +1,31 @@
 import { Link } from "react-router-dom";
-
+import img1 from "../../assets/Blog/img1.jpg"
+import img2 from "../../assets/Blog/img2.jpg"
+import img3 from "../../assets/Blog/img3.jpg"
 
 const Blog = () => {
     return (
-        <div className="grid grid-cols-3 justify-items-center m-20">
+        <div className="grid grid-cols-3 justify-items-center m-10 gap-10">
             <div className="bg-blue-200 p-20 rounded-lg">
-                <Link to="https://legacy.reactjs.org/docs/context.html#when-to-use-context"><h2>What is Context API?</h2></Link>
+                <Link to="https://legacy.reactjs.org/docs/context.html#when-to-use-context"target="_blank">
+                    <h2>What is Context API?</h2>
+                    <img className="h-60" src={img1} alt="" />
+                </Link>
+                
             </div>
             <div className="bg-blue-200 p-20 rounded-lg">
-                <Link to="https://legacy.reactjs.org/docs/hooks-intro.html"><h2>What is React Hooks?</h2></Link>
+                <Link to="https://legacy.reactjs.org/docs/hooks-intro.html" target="_blank">
+                    <h2>What is React Hooks?</h2>
+                    <img className="h-60" src={img2} alt="" />
+                </Link>
+                
             </div>
             <div className="bg-blue-200 p-20 rounded-lg">
-                <Link to="https://legacy.reactjs.org/docs/hooks-intro.html"><h2>What is React?</h2></Link>
+                <Link to="https://legacy.reactjs.org/docs/hooks-intro.html"target="_blank">
+                    <h2>What is React?</h2>
+                    <img className="h-60 object-cover" src={img3} alt="" />
+                 </Link>
+                
             </div>
         </div>
     );
