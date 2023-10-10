5613dee5335445ffcde9930ae663005f3376ceed
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-10 12:00:51 +0600

    update blog section

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
 
 
 const Blog = () => {
     return (
-        <div>
-           <h2>This is blog section</h2> 
+        <div className="grid grid-cols-3 justify-items-center m-20">
+            <div className="bg-blue-200 p-20 rounded-lg">
+                <Link to="https://legacy.reactjs.org/docs/context.html#when-to-use-context"><h2>What is Context API?</h2></Link>
+            </div>
+            <div className="bg-blue-200 p-20 rounded-lg">
+                <Link to="https://legacy.reactjs.org/docs/hooks-intro.html"><h2>What is React Hooks?</h2></Link>
+            </div>
+            <div className="bg-blue-200 p-20 rounded-lg">
+                <Link to="https://legacy.reactjs.org/docs/hooks-intro.html"><h2>What is React?</h2></Link>
+            </div>
         </div>
     );
 };
