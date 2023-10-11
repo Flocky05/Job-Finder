b5e29c4ed07ec89435a3cbff4c7099e66049a49d
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-11 21:31:07 +0600

    added jobContext

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -5,22 +5,22 @@ import img3 from "../../assets/Blog/img3.jpg"
 
 const Blog = () => {
     return (
-        <div className="grid grid-cols-3 justify-items-center m-10 gap-10">
-            <div className="bg-blue-200 p-20 rounded-lg">
+        <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center m-5 lg:m-10 gap-10">
+            <div className="bg-blue-200 p-10 lg:p-20 rounded-lg">
                 <Link to="https://legacy.reactjs.org/docs/context.html#when-to-use-context"target="_blank">
                     <h2>What is Context API?</h2>
                     <img className="h-60" src={img1} alt="" />
                 </Link>
                 
             </div>
-            <div className="bg-blue-200 p-20 rounded-lg">
+            <div className="bg-blue-200 p-10 lg:p-20 rounded-lg">
                 <Link to="https://legacy.reactjs.org/docs/hooks-intro.html" target="_blank">
                     <h2>What is React Hooks?</h2>
                     <img className="h-60" src={img2} alt="" />
                 </Link>
                 
             </div>
-            <div className="bg-blue-200 p-20 rounded-lg">
+            <div className="bg-blue-200 p-10 lg:p-20 rounded-lg">
                 <Link to="https://legacy.reactjs.org/docs/hooks-intro.html"target="_blank">
                     <h2>What is React?</h2>
                     <img className="h-60 object-cover" src={img3} alt="" />
