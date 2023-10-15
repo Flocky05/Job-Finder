9f20ba9d31d9b5c11341efc80080073eacc13aa4
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-10-16 00:02:30 +0600

    make responsive the jobCamping section

--- src/Components/JobCamping/JobCamping.jsx
+++ src/Components/JobCamping/JobCamping.jsx
@@ -4,26 +4,29 @@ import img3 from "../../assets/jobCamping/img3.jpg"
 
 const JobCamping = () => {
     return (
-        <div className="grid grid-cols-3 gap-10">
-            <div className=" bg-yellow-100 m-10  p-10  rounded-md">
-                <img src={img1} alt="" />
-                <div>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus consequatur inventore modi at error aspernatur voluptates sit iure iste impedit.</p>
-                    <button className="bg-orange-500 px-6 py-2 rounded-lg text-white mt-3">learn More</button>
+        <div>
+            <h2 className="text-center text-3xl p-10 uppercase font-bold">JOb <span className="text-blue-400">Camping</span> 2023</h2>
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+                <div className=" bg-blue-100 m-10  p-10  rounded-md">
+                    <img src={img1} alt="" />
+                    <div>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus consequatur inventore modi at error aspernatur voluptates sit iure iste impedit.</p>
+                        <button className="bg-blue-500 px-6 py-2 rounded-lg text-white mt-3">learn More</button>
+                    </div>
                 </div>
-            </div>
-            <div className=" bg-yellow-100 m-10  p-10  rounded-md">
-                <img src={img2} alt="" />
-                <div>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus consequatur inventore modi at error aspernatur voluptates sit iure iste impedit.</p>
-                    <button className="bg-orange-500 px-6 py-2 rounded-lg text-white mt-3">learn More</button>
+                <div className=" bg-blue-100 m-10  p-10  rounded-md">
+                    <img src={img2} alt="" />
+                    <div>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus consequatur inventore modi at error aspernatur voluptates sit iure iste impedit.</p>
+                        <button className="bg-blue-500 px-6 py-2 rounded-lg text-white mt-3">learn More</button>
+                    </div>
                 </div>
-            </div>
-            <div className=" bg-yellow-100 m-10  p-10  rounded-md">
-                <img src={img3} alt="" />
-                <div>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus consequatur inventore modi at error aspernatur voluptates sit iure iste impedit.</p>
-                    <button className="bg-orange-500 px-6 py-2 rounded-lg text-white mt-3">learn More</button>
+                <div className=" bg-blue-100 m-10  p-10  rounded-md">
+                    <img src={img3} alt="" />
+                    <div>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus consequatur inventore modi at error aspernatur voluptates sit iure iste impedit.</p>
+                        <button className="bg-blue-500 px-6 py-2 rounded-lg text-white mt-3">learn More</button>
+                    </div>
                 </div>
             </div>
         </div>
